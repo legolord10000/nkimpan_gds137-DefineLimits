ce70f0eba1f8953dcc161087d2f1fc60dfa01ad0
Nathan Kimpan <nathankimpan@gmail.com>
2026-04-23 20:20:24 -0400

    testing

--- js/gameloop.js
+++ js/gameloop.js
@@ -9,8 +9,11 @@ canvas = document.getElementById("canvas");
 context = canvas.getContext("2d");
 
 player = new GameObject(1,canvas.height/2,5,100,"#6200ff");
+player2 = new GameObject(canvas.height,canvas.height/2,50,100,"#6200ff");
 player.vx=0;
-player.vy=-4;
+player.vy=-0;
+player2.vx=-4;
+player2.vy=0;
 
 npc1 = new GameObject(300,canvas.height/2,100,100,"#00ff00")
 npc2 = new GameObject(600,canvas.height/2,100,100,"#0000ff")
@@ -25,19 +28,19 @@ function animate()
     
     if (d)
     {
-        player.x += 4
+        player2.x += 4
     }
     if (a)
     {
-        player.x -= 4
+        player2.x -= 4
     }
     if (w)
     {
-        player.y += 4
+        player2.y += 4
     }
     if (s)
     {
-        player.y -= 4
+        player2.y -= 4
     }
 
     player.move();
@@ -59,15 +62,45 @@ function animate()
         }
         if (player.y > canvas.height - player.height/2)
         {
-            player.vy=-4
+            //player.vy=-4
             //player.color = "#00ff00"
             player.y = canvas.height -player.height/2
         }
         else if (player.y < player.height/2)
         {
-            player.vy=4
+            //player.vy=4
             //player.color = "#ff0000" 
             player.y = player.height/2
+        }
+    
+        player2.move();
+        
+        
+        if (player2.x > canvas.width + player2.width/2)
+        {
+            player2.vx=-4
+            //player.color = "#ffff00"
+            player2.x = -player2.width/2;
+
+        }
+        else if (player2.x < -player2.width/2)
+        {
+            player2.vx=4
+            //player.color = "#0000ff"
+            player2.x = canvas.width + player2.width/2;
+
+        }
+        if (player2.y > canvas.height - player2.height/2)
+        {
+            player2.vy=-4
+            //player.color = "#00ff00"
+            player2.y = canvas.height -player2.height/2
+        }
+        else if (player2.y < player2.height/2)
+        {
+            player2.vy=4
+            //player.color = "#ff0000" 
+            player2.y = player2.height/2
         }
         
     
@@ -97,6 +130,7 @@ function animate()
         }
 
     player.drawRect();   
+    player2.drawCircle();  
     //npc1.drawCircle();  
     //npc2.drawCircle();  
     //npc3.drawRect();  
